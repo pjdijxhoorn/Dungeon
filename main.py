c6425c4faa7d509e0247163e43644373732c031d
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2024-01-15 11:47:15 +0100

    <SETUP> Connection database

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
+
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import psycopg2
 
 app = FastAPI()
-
+load_dotenv()
 
 @app.get("/")
 async def root():
@@ -11,3 +15,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+db_connection = psycopg2.connect(
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),  # Of het IP-adres van je databasehost
+    port=os.getenv("DB_PORT")  # De standaard PostgreSQL-poort
+)
