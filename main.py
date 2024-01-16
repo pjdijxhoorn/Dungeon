7a28eab5acb5faec32bd54ce84a441769a1f47ee
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2024-01-16 12:31:07 +0100

    <FEAT> made basic get and post path

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import os
-
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import psycopg2
 
 app = FastAPI()
 load_dotenv()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Player(BaseModel):
+    player_id: int
+    username: str
+    name: str
+    average_score: int
+    training_score: list[int]
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/user")
+async def getUsers():
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM player")
+    data = cursor.fetchall()
+    print(data)
+    cursor.close()
+    return {"data": data}
+
+@app.post("/user")
+async def create_user(player: Player):
+    cursor = db_connection.cursor()
+    cursor.execute(
+        'INSERT INTO player (player_id, username, name, average_score, training_score) VALUES (%s, %s, %s, %s, %s)',
+        (player.player_id, player.username, player.name, player.average_score, player.training_score)
+    )
+    db_connection.commit()
+    return {"player": player}
+
 
 db_connection = psycopg2.connect(
     database=os.getenv("DB_NAME"),
