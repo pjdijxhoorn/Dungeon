5a2bf34654213bd9901118b298329379a383bca1
Anissanovi <anissa.issarti@novi-education.nl>
2024-01-23 17:28:32 +0100

    <FEAT> Made model of profile

--- app/models/profile.py
+++ app/models/profile.py
@@ -1,18 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, DateTime, Numeric
+from sqlalchemy import Column, Integer, DateTime, Float
 
 Base = declarative_base()
 
 class Profile(Base):
-    __tablename__ = "profile"
+    __tablename__ = "fitness_profile"
     profile_id = Column(Integer, primary_key=True)
-    bmi = Column(Numeric)
-    bmr = Column(Numeric)
-    weight = Column(Numeric)
-    height = Column(Numeric)
+    bmi = Column(Float)
+    weight_in_kg = Column(Float)
+    height_in_m = Column(Float)
     date_of_birth = Column(DateTime)
-    max_hf = Column(Integer)  # hf stands for "Heart frequency"
-    base_hf = Column(Integer)
-    reserve_hf = Column(Integer)
+    max_heart_frequency = Column(Integer)  # hf stands for "Heart frequency"
+    rest_heart_frequency = Column(Integer)
+    reserve_heart_frequency = Column(Integer)
 
     
