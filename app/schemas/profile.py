e5cfac234e165e336dc83a3822440b12a3fef155
Anissanovi <anissa.issarti@novi-education.nl>
2024-01-23 17:29:51 +0100

    <FEAT> Made schemas of profile

---
+++ app/schemas/profile.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import date
+
+
+class BaseProfile(BaseModel):
+    weight_in_kg: float
+    height_in_m: float
+    rest_heart_frequency: int
+
+class Profile(BaseProfile):
+    #model_config = ConfigDict #(from_attributes=True)
+    profile_id: int
+    bmi: float
+    weight_in_kg: float
+    height_in_m: float
+    date_of_birth: date
+    max_heart_frequency: int  # hf stands for "Heart frequency"
+    rest_heart_frequency: int
+    reserve_heart_frequency: int
+
+class CreateProfile(BaseProfile):
+    #model_config = ConfigDict #(from_attributes=True)
+    weight_in_kg: float
+    height_in_m: float
+    date_of_birth: date
+    rest_heart_frequency: int
+
+class UpdateProfile(BaseProfile):
+    #model_config = ConfigDict #(from_attributes=True)
+    weight_in_kg: float
+    height_in_m: float
+    rest_heart_frequency: int
+
+
+#we calculate bmi and different HF by ourself without updating it here.
