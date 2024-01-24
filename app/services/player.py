e4eb7725e779f96f145c347b8a4502a82d654993
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2024-01-24 21:58:09 +0100

    <Feat> Added password to createplayer and made login service

--- app/services/player.py
+++ app/services/player.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.models.player import Player
-
+from passlib.context import CryptContext
 
 def get_players(db: Session):
     return db.query(Player).all()
@@ -13,7 +13,9 @@ def get_player(db: Session, player_id: int):
 
 
 def create_player(player, db: Session):
+
     db_Player = Player(username=player.username,
+                       password=get_password_hash(player.password),
                        name=player.name,
                        average_score=0,
                        training_score=[0])
@@ -44,3 +46,25 @@ def patch_player(player_id: int, update_player, db: Session):
     db.commit()
     db.refresh(player)
     return player
+
+
+def login(player_login, db):
+    player = db.query(Player).filter(Player.username == player_login.username).first()
+    if player is None:
+        raise HTTPException(status_code=404, detail="Incorrect username and/ or  password")
+    if not verify_password(player_login.password, player.password):
+        raise HTTPException(status_code=404, detail="Incorrect username and/ or  password")
+
+    # todo make jwt token
+    # todo build logout
+    # todo add checks to all routes for own entities and login
+
+    return f"welcome {player.username}"
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
