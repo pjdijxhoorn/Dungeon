6f32aaace26b6ae5ecefe782720e9909aa99cd16
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2024-01-25 13:51:50 +0100

    <FEAT> Added authentication routers

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from app.routers import player, profile, training
+from app.routers import player, profile, training, authentication
 from database import get_db
 from fastapi import Depends
-
 app = FastAPI()
 
 app.include_router(player.router, prefix="/player")
 app.include_router(profile.router, prefix="/profile")
 app.include_router(training.router, prefix="/training")
+app.include_router(authentication.router, prefix="/authentication")
 
 
 @app.on_event("startup")
