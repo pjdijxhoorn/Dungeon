a551dd5edfda64509e2ce3b951c5320dbda69cac
Anissanovi <anissa.issarti@novi-education.nl>
2024-01-25 14:25:06 +0100

    <FEAT> Deleted router.delete in profile and training

--- app/routers/profile.py
+++ app/routers/profile.py
@@ -5,33 +5,23 @@ from database import get_db
 
 router = APIRouter()
 
-#all profiles
+
 @router.get("",status_code=200, tags=["Profile"])
 def get_profiles(db=Depends(get_db)) -> list[Profile]:
     profiles = services.get_profiles(db)
     return profiles
 
 @router.get("/{profile_id}", status_code=200,  tags=["Profile"])
-#one profile
 def get_profile(profile_id: int, db=Depends(get_db)) -> Profile:
     profile = services.get_profile(db, profile_id)
     if profile is None:
         raise HTTPException(status_code=404, detail="profile not found")
     return profile
 
-#create profile
 @router.post("", status_code=201, tags=["Profile"])
-
 def create_profile(profile: CreateProfile, db=Depends(get_db)) -> Profile:
     return services.create_profile(profile, db)
 
-@router.delete("/{profile_id}", status_code=200, tags=["Profile"])
-
-def delete_profile(profile_id: int, db=Depends(get_db)):
-    message = services.delete_profile(profile_id, db)
-    return message
-#update profile
 @router.put("/{profile_id}", status_code=200, tags=["Profile"])
-
 def patch_profile(profile_id: int, profile: UpdateProfile, db=Depends(get_db)):
     return services.patch_profile(profile_id, profile, db)
