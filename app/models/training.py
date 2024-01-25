ac2f47ad9ca7da309ef6f1b98b8a7e81c997f344
Anissanovi <anissa.issarti@novi-education.nl>
2024-01-25 14:17:51 +0100

    <FEAT> Added player_id to profile and training models

--- app/models/training.py
+++ app/models/training.py
@@ -6,6 +6,7 @@ Base = declarative_base()
 class Training(Base):
     __tablename__ = "training"
     training_id = Column(Integer, primary_key=True)
+    player_id = Column(Integer)
     training_name = Column(String, default="endurance")
     distance_in_meters = Column(Integer)
     time_in_seconds = Column(Integer)
@@ -13,6 +14,7 @@ class Training(Base):
     training_type = Column(String)
     base_score = Column(Integer) 
     training_date = Column(Date)
+
 
 
     
