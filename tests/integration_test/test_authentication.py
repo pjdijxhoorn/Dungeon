e0e3ddab0ec53b68d12b4bf1440a83b3eb383e2f
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2024-01-28 21:36:16 +0100

    <Test> added HTTPExecption tests

--- tests/integration_test/test_authentication.py
+++ tests/integration_test/test_authentication.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 
+from app.services.authentication import authenticate_user
 from database import get_db
 from main import app
 from fastapi.testclient import TestClient
@@ -14,3 +15,19 @@ def test_login():
     db: Session = next(get_db())
     response = client.post("/authentication/login", data=login_data, params={"db": db})
     assert response.status_code == 200
+
+
+def test_login_incorrect_login():
+    login_data = {
+        "username": "user4",
+        "password": "wachtwoord",
+    }
+    db: Session = next(get_db())
+    response = client.post("/authentication/login", data=login_data, params={"db": db})
+    assert response.status_code == 401
+    assert  response.json() == {'detail': 'Incorrect username or password'}
+
+def test_authenticate_user():
+    db: Session = next(get_db())
+    user = authenticate_user(db, "he who must not be named", '$2a$12$1b1hXeBeAL5rNsDeDdFsUu8VLdcA/ZE9IBEUug9b6kmn8t28alPFG')
+    assert user == False
