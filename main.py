094d8fb4f3460080724555cadedb1fbe195855ee
Anissanovi <anissa.issarti@novi-education.nl>
2024-01-30 10:20:50 +0100

    <FIX> commented until further notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import player, profile, training, authentication
+from app.routers import player, profile, training #, authentication
 from database import get_db
 from fastapi import Depends
 app = FastAPI()
@@ -7,7 +7,7 @@ app = FastAPI()
 app.include_router(player.router, prefix="/player")
 app.include_router(profile.router, prefix="/profile")
 app.include_router(training.router, prefix="/training")
-app.include_router(authentication.router, prefix="/authentication")
+#app.include_router(authentication.router, prefix="/authentication")
 
 
 @app.on_event("startup")
