820e4f03432d5d5bbcdd136dfc8fc373cf899c58
samohtww <32458561+samohtww@users.noreply.github.com>
2024-02-05 19:39:11 +0100

    <FEATURE> added terraform in the CI/CD

---
+++ config/setup/grafana-loki.tf
@@ -0,0 +1,53 @@
+data "digitalocean_kubernetes_cluster" "dungeon-run" {
+  name = "dungeon-run-cluster"
+  depends_on = [digitalocean_kubernetes_cluster.dungeon-run]
+}
+
+provider "kubernetes" {
+  host  = data.digitalocean_kubernetes_cluster.dungeon-run.endpoint
+  token = data.digitalocean_kubernetes_cluster.dungeon-run.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.dungeon-run.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.dungeon-run.endpoint
+    token = digitalocean_kubernetes_cluster.dungeon-run.kube_config[0].token
+
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.dungeon-run.kube_config[0].cluster_ca_certificate
+    )
+  }
+}
+
+resource "kubernetes_namespace" "loki-stack" {
+  metadata {
+    annotations = {
+      name = "loki-stack"
+    }
+
+    name = "loki-stack"
+  }
+  depends_on = [digitalocean_kubernetes_cluster.dungeon-run]
+}
+
+resource "helm_release" "loki" {
+  name       = "loki"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki-stack"
+  version    = "2.10.1"
+  namespace = "loki-stack"
+
+  set {
+    name  = "grafana.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "promtail.enabled"
+    value = "true"
+  }
+  depends_on = [kubernetes_namespace.loki-stack]
+}
