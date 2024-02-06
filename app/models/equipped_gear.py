96e5a8dd28dcb6c5153260fe43cc5f6a672c3f1b
samohtww <32458561+samohtww@users.noreply.github.com>
2024-02-06 10:31:12 +0100

    <FEATURE> updated the models and added new ones

--- app/models/equipped_gear.py
+++ app/models/equipped_gear.py
@@ -4,10 +4,11 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
-class Gear(Base):
-    """ Represents a item a player can use. """
-    __tablename__ = "gear"
+class EquippedGear(Base):
+    """ Represents a item a player has equipped. """
+    __tablename__ = "Equipped_gear"
     gear_id = Column(Integer, primary_key=True)
     gear_name = Column(String, default="gear")
     gear_class = Column(String)
     gear_slot = Column(String)
+    gear_price = Column(Integer)
