8adb5b62bf8678b0d002302f5ca7418e6376b667
Anissanovi <anissa.issarti@novi-education.nl>
2024-02-06 12:24:34 +0100

    <FEAT> Added gear_stat and type to gear model

--- app/models/gear.py
+++ app/models/gear.py
@@ -8,6 +8,9 @@ class Gear(Base):
     """ Represents a item a player can use. """
     __tablename__ = "gear"
     gear_id = Column(Integer, primary_key=True)
-    gear_name = Column(String, default="gear")
+    gear_name = Column(String)
     gear_class = Column(String)
     gear_slot = Column(String)
+    gear_stat_type = Column(String)
+    gear_stat = Column(Integer)
+    gear_price = Column(Integer)
