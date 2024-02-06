b2de4831fd4cba5026b79aa7b639db197644a663
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2024-02-06 14:02:07 +0100

    <FEAT> build a leveling system

--- app/services/dungeon_run.py
+++ app/services/dungeon_run.py
@@ -15,9 +15,9 @@ def get_Dungeon_run():
         # meerder beurten totdat of jij of het monster dood is monster moet dezelfde stats hebben?
         # monster dood loot berekenen en toevoegen aan speler? aan het einde ?
         # berekenen opgedane xp
-    # einde van de dungeon zonder dood te gaan xp genoeg om te levelen?
+    # einde van de dungeon te gaan xp genoeg om te levelen?
     # loot erbij
-    # bonus voor bereiken einde dungeon
+    # bonus voor bereiken einde dungeon zonder dood te gaan
     # titles toekennen aan de speler
     return "hello"
 
@@ -33,4 +33,21 @@ def get_monster():
 # gebaseerd op stats
 
 # def get monster block
+def gain_xp(base_stats, amount):
+    base_stats.xp += amount
+    # print(f"{base_stats.name} gained {amount} XP!")
 
+    # Check for level up
+    while base_stats.xp >= calculate_xp_required(base_stats):
+        level_up(base_stats)
+    # todo increase base stats
+
+def level_up(base_stats):
+    base_stats.level += 1
+    remaining_xp = base_stats.xp - calculate_xp_required()
+    base_stats.xp = 0 if remaining_xp < 0 else remaining_xp
+   # print(f"{self.name} leveled up to level {self.level}!")
+
+
+def calculate_xp_required(base_stats):
+    return 100 + (base_stats.level-1) ** 3
