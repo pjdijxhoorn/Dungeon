9ce8b30a9911317a6e76e583a193dd6f5f8ad378
Anissanovi <anissa.issarti@novi-education.nl>
2024-02-06 12:27:12 +0100

    <FEAT> Updated player base stats model

---
+++ app/models/player_base_stats.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer
+
+Base = declarative_base()
+
+class PlayerBaseStats(Base):
+    """ Represents a players base stats used for dungeon run's. """
+    __tablename__ = "player_base_stats"
+    player_base_stats_id = Column(Integer, primary_key=True)
+    player_id = Column(Integer)
+    strenght = Column(Integer)
+    defense = Column(Integer)
+    speed = Column(Integer)
+    health = Column(Integer)
+    accuracy = Column(Integer)
+    player_level = Column(Integer)
