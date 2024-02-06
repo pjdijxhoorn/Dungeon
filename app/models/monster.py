5459c497e4b7e4635bb4f57f8495b39e331181bc
samohtww <32458561+samohtww@users.noreply.github.com>
2024-02-06 10:11:53 +0100

    <FEAT> added models for dungeon run

---
+++ app/models/monster.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class Monster(Base):
+    """ Represents a monster that the player is able to encounter in a dungeon run. """
+    __tablename__ = "monster"
+    monster_id = Column(Integer, primary_key=True)
+    monster_name = Column(String, default="monster")
+    base_strength = Column(Integer)
+    distance_strength = Column(Integer)
