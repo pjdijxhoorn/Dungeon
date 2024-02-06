39c323d45cebfac831dfa9e0aef99c6dd3b6f2c4
Anissanovi <anissa.issarti@novi-education.nl>
2024-02-06 12:23:43 +0100

    <FEAT> Updated equipped_gear model

--- app/models/equipped_gear.py
+++ app/models/equipped_gear.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer
 
 Base = declarative_base()
 
@@ -7,8 +7,10 @@ Base = declarative_base()
 class EquippedGear(Base):
     """ Represents a item a player has equipped. """
     __tablename__ = "Equipped_gear"
-    gear_id = Column(Integer, primary_key=True)
-    gear_name = Column(String, default="gear")
-    gear_class = Column(String)
-    gear_slot = Column(String)
-    gear_price = Column(Integer)
+    equipped_gear_id = Column(Integer, primary_key=True)
+    player_id = Column(Integer)
+    equipped_slot_head = Column(Integer)
+    equipped_slot_weapon = Column(Integer)
+    equipped_slot_armor = Column(Integer)
+    equipped_slot_boots = Column(Integer)
+    equipped_slot_title = Column(Integer)
