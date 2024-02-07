bb0f7ed6416b94b4469214d4695a70ecbda8054e
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2024-02-07 11:57:47 +0100

    <FEAT> gave extra stats to monsters

--- app/models/monster.py
+++ app/models/monster.py
@@ -7,8 +7,12 @@ Base = declarative_base()
 class Monster(Base):
     """ Represents a monster that the player is able to encounter in a dungeon run. """
     __tablename__ = "monsters"
+
     monster_id = Column(Integer, primary_key=True)
     monster_name = Column(String)
-    base_strength = Column(Integer)
-    distance_strength = Column(Integer)
+    defence = Column(Integer)
+    strength = Column(Integer)
+    health = Column(Integer)
+    speed = Column(Integer)
+    accuracy = Column(Integer)
     zone_difficulty = Column(String)
