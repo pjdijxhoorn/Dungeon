2f7e223573bfc95d78b7d891a78aaad53a22b721
Anissanovi <anissa.issarti@novi-education.nl>
2024-02-09 17:00:43 +0100

    <FEAT> Tweaked some parts to make it run so i could test

--- app/routers/dungeon_run.py
+++ app/routers/dungeon_run.py
@@ -13,12 +13,12 @@ def get_dungeon_run(training_id: int, player_id: int, db=Depends(get_db)):
     return services.get_dungeon_run(training_id, player_id, db)
 
 
-@router.get("/temp_player", status_code=200, tags=["Dungeon_run"])
-def get_temporary_player(training_id: int, player_id: int, db=Depends(get_db)):
-    """ Router for the function to get a temporary player. """
-    return services.get_temporary_player(training_id, player_id, db)
+#@router.get("/temp_player", status_code=200, tags=["Dungeon_run"])
+#def get_temporary_player(training_id: int, player_id: int, db=Depends(get_db)):
+#    """ Router for the function to get a temporary player. """
+#    return services.get_temporary_player(training_id, player_id, db)
 
 
 @router.get("/clan", status_code=200, tags=["Dungeon_run"])
-def get_clan_dungeon_run():
-    return service.get_dungeon_run_clan()
+def get_clan_dungeon_run(player_ids, db=Depends(get_db)):
+    return service.get_dungeon_run_clan(player_ids, db)
