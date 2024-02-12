4822df20c60ef3a712ecb67d4929e73e3dbd9cff
Anissanovi <anissa.issarti@novi-education.nl>
2024-02-12 11:35:31 +0100

    <FEAT> Added model for future temp_monster function

---
+++ app/models/temp_monster.py
@@ -0,0 +1,12 @@
+class TempMonster:
+    """ A class for the temporary monster. """
+
+    def __init__(self, name, strenght, defence, speed, accuracy, health, zone_difficulty, play_status=True):
+        self.name = name
+        self.strenght = strenght
+        self.defence = defence
+        self.speed = speed
+        self.accuracy = accuracy
+        self.health = health
+        self.zone_difficulty = zone_difficulty
+        self.play_status = play_status
