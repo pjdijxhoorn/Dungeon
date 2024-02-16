1caf3211b98909e8a86d414d55249e8168990d46
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2024-02-16 16:32:42 +0100

    <FIX> changed get and post dungeon run to put

--- tests/integration_test/test_dungeon_run.py
+++ tests/integration_test/test_dungeon_run.py
@@ -6,10 +6,10 @@ from main import app
 client = TestClient(app)
 
 
-def test_get_dungeon_run():
+def test_put_dungeon_run():
     """ Test the endpoint for dungeon run """
     reset_database()
-    response = client.get("/dungeon_run/1/1")
+    response = client.put("/dungeon_run/1/1")
     assert response.status_code == 200
     assert response.text.strip() != ''
     response_text = response.text.lower()
@@ -18,27 +18,27 @@ def test_get_dungeon_run():
     assert 'encountered' in response_text
 
 
-def test_get_dungeon_run_player_not_found():
+def test_put_dungeon_run_player_not_found():
     """ Test the endpoint for dungeon run """
-    response = client.get("/dungeon_run/1/999")
+    response = client.put("/dungeon_run/1/999")
     assert response.status_code == 404
     assert response.json() == {
         "detail": "Player not found"
     }
 
 
-def test_get_dungeon_run_training_not_found():
+def test_put_dungeon_run_training_not_found():
     """ Test the endpoint for dungeon run """
-    response = client.get("/dungeon_run/999/1")
+    response = client.put("/dungeon_run/999/1")
     assert response.status_code == 404
     assert response.json() == {
         "detail": "training not found"
     }
 
 
-def test_get_dungeon_run_training_not_useable():
+def test_put_dungeon_run_training_not_useable():
     """ Test the endpoint for dungeon run """
-    response = client.get("/dungeon_run/15/14")
+    response = client.put("/dungeon_run/15/14")
     assert response.status_code == 404
     assert response.json() == {
         'detail': 'training already used for a dungeon run'}
